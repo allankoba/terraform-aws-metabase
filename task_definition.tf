d90791ff9feef009a2a10b05da838812b9568e23
andrew misiti <andrew.misiti@gmail.com>
2025-08-11 11:01:47 -0400

    Revert readonly filesystem

--- task_definition.tf
+++ task_definition.tf
@@ -10,7 +10,7 @@ resource "aws_ecs_task_definition" "task_definition" {
       "image" : "metabase/metabase"
       "essential" : true
       "memoryReservation" : var.ecs_task_memory
-      "readonlyRootFilesystem": true
+      //"readonlyRootFilesystem": true
       "cpu" : var.ecs_task_cpu
 
       "logConfiguration" : {
